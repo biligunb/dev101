411379a912ee5a100f288987692581f998ce2ca5
Altanmur Tumurbat (Алтанмөр) <altanmor@gmail.com>
2023-02-27 22:26:55 +0800

    [FIX] formatting same order

--- week1/Altanmur/Tasks/D6_developer_tools.js
+++ week1/Altanmur/Tasks/D6_developer_tools.js
@@ -1,67 +1,65 @@
 developer_tools = {
-    document : {
-        google: {
-            task: "google sheet",
-            video_conference: "google meet",
-            document: "google docs",
-            survey: "google form",
-            presentation: "googl slide",
-            store: "google drive",
-            email: "google gmail"
+    "google": {
+        "task": "google sheet",
+        "video_conference": "google meet",
+        "document": "google docs",
+        "survey": "google form",
+        "presentation": "googl slide",
+        "store": "google drive",
+        "email": "google gmail"
+    },
+    "chat": {
+        "slack": "connect webhook"
+    },
+    "code_source": {
+        "git": "gitlab",
+        "project": "gitlab issue"
+    },
+    "editor": {
+        "VSCode": {
+            "coding": "write code, support all programming language",
+            "extension": ["docker", "postman/thunder","sqlclient","git"],
+            "markdown": ["doc","wiki"]
         },
-        chat: {
-            slack: "connect webhook"
-        },
-        code_source: {
-            git: "gitlab",
-            project: "gitlab issue"
-        },
-        editor: {
-            VSCode: {
-                coding: "write code, support all programming language",
-                extension: ["docker", "postman/thunder","sqlclient","git"],
-                markdown: ["doc","wiki"]
-            },
-            terminal: ["yakuake"],
-            sql: {
-                MySQL: "workbench"
-            }
-        },
-        documention: {
-            use: ["screenshot","screenrecord","video to gif"],
-            tools: ["kazam","ezgif.com"]
+        "terminal": ["yakuake"],
+        "sql": {
+            "MySQL": "workbench"
         }
+    },
+    "documention": {
+        "use": ["screenshot","screenrecord","video to gif"],
+        "tools": ["kazam","ezgif.com"]
     }
 }
 now_using_tools = {
-    chat: ["Microsoft Teams", "odoo chat (internal system)","fb messenger"],
-    google: {
-        data_clean: "google sheet",
-        document: "google docs",
-        presentation: "googl slide",
-        store: "google drive",
-        email: "google gmail"
+    "google": {
+        "data_clean": "google sheet",
+        "document": "google docs",
+        "presentation": "googl slide",
+        "store": "google drive",
+        "email": "google gmail"
     },
-    code_source: {
-        git: {
-            work: "Dev Azure",
-            self: "gitlab"
+    "chat": ["Microsoft Teams", "odoo chat (internal system)","fb messenger"],
+    "code_source": {
+        "git": {
+            "work": "Dev Azure",
+            "self": "gitlab"
         },
-        project: "Microsoft Dev Azure"
+        "project": "Microsoft Dev Azure"
     },
-    editor: {
-        VSCode: {
-            coding: ["python", "xml", "js", "css"],
-            extension: ["odoo formatter"],
+    "editor": {
+        "VSCode": {
+            "coding": ["python", "xml", "js", "css"],
+            "extension": ["odoo formatter"]
         },
-        terminal: ["yakuake"],
-        sql: {
-            PostgreSQL: "pgAdmin"
+        "terminal": ["yakuake"],
+        "sql": {
+            "PostgreSQL": "pgAdmin"
         }
     },
-    OS: "MacOS",
-    documention: {
-        use: ["ss", "screenrecord"],
-        tools: ["quicktime"]
-    }
+    "documention": {
+        "use": ["ss", "screenrecord"],
+        "tools": ["quicktime"]
+    },
+    "OS": "MacOS"
 }
