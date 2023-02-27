627f77ddc5917430996b4e6cfcb7ebc6d8708cdf
Altanmur Tumurbat (Алтанмөр) <altanmor@gmail.com>
2023-02-27 22:07:50 +0800

    [ADD] D6 Tools of Developer

---
+++ week1/Altanmur/Tasks/D6_developer_tools.js
@@ -0,0 +1,37 @@
+developer_tools = {
+    document : {
+        google: {
+            task: "google sheet",
+            video_conference: "google meet",
+            document: "google docs",
+            survey: "googl form",
+            presentation: "googl slide",
+            store: "google drive",
+            email: "google gmail"
+        },
+        chat: {
+            slack: "connect webhook"
+        },
+        code_source: {
+            git: "gitlab",
+            project: "gitlab issue"
+        },
+        editor: {
+            VSCode: {
+                coding: "write code, support all programming language",
+                extension: ["docker", "postman/thunder","sqlclient","git"],
+                markdown: ["doc","wiki"],
+
+            },
+            terminal: ["yakuake"],
+            sql: {
+                MySQL: "workbench",
+                PostgreSQL: "pyAdmin"
+            }
+        },
+        documention: {
+            use: ["screenshot","screenrecord","video to gif"],
+            tools: ["kazam","ezgif.com"]
+        }
+    }
+}
