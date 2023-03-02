3f67f2624e4def952d8bbd003a92376a6478f4ea
Grimstar15 <ttsegtsee@gmail.com>
2023-03-02 17:39:33 +0800

    day9 task added

--- week1/tsegts-erdene/portfolio/src/Home.js
+++ week1/tsegts-erdene/portfolio/src/Home.js
@@ -1,18 +1,18 @@
 import React from 'react'
-
+import './Home.css'
 const Home = () => {
   return (
     <div className='container'>  
-        <p align="center">
+        <h1>
     Цэгц-Эрдэнэ Гантулга
-</p>
-<p>
-    Цахим хаяг: tsegtserdene.gantulga@gmail.com Байршил: Улаанбаатар
-</p>
-<h1>
-    Товч мэдээлэл
 </h1>
-<p>
+<p className='email'>
+    <span>Цахим хаяг: tsegtserdene.gantulga@gmail.com</span><span>Байршил: Улаанбаатар</span>
+</p>
+<h2>
+    Товч мэдээлэл
+</h2>
+<p className='aboutme'>
     Миний бие Гантулга овогтой Цэгц-Эрдэнэ нь МУИС ХШУИС-ын сүлжээний ангийн
     оюутан. 2017 оноос хойш энэхүү ангид сурч байгаа бөгөөд уг хугацаанд
     өөрийгөө software хөгжүүлэлтэнд илүү дуртай сонирхолтой гэдгээ мэдэж авсан.
@@ -28,129 +28,121 @@ const Home = () => {
 <p>
     <strong>Empasoft LLC </strong>
 </p>
-<p>
+<ul>
+<li>
     <em>Business process automation engineer, Developer, Trainer </em>
-</p>
-<p>
-    Ø Боловсролын яамтай хамтран Монгол Улсын төрийн бүх бүрэн дунд сургууль,
+</li>
+<li>
+    Боловсролын яамтай хамтран Монгол Улсын төрийн бүх бүрэн дунд сургууль,
     цэцэрлэг, боловсролын газарт office 365 админ болон хэрэглээний сургалт
     орсон. /102 цагийн 9304 хүн/
-</p>
-<p>
-    Ø АПУ ХК дотоод ажилчдын office 365 cloud дээрх аюулгүй байдлыг хангах
+</li>
+<li>
+    АПУ ХК дотоод ажилчдын office 365 cloud дээрх аюулгүй байдлыг хангах
     Intune нэвтрүүлэлт /end-user security awareness/
-</p>
-<p>
-    Ø Microsoft power platform ашиглан Мобиком ХХК-н цахим нэрийн хуудас
+</li>
+<li>
+    Microsoft power platform ашиглан Мобиком ХХК-н цахим нэрийн хуудас
     автоматжуулалтыг хийсэн
-</p>
-<p>
-    Ø Эмпасофт шинэ веб сайтыг хөгжүүлсэн. / Front end, Back end/
-</p>
-<p>
-    Ø Цаг бүртгэлийн автоматжуулалт /Power Platform/
-</p>
-<p>
-    Ø Online сургалтын платформ /Power Platform/
-</p>
-<p>
-    Ø Inspire 2022 арга хэмжээнд зохион байгуулагч, илтгэгч, сүлжээний
+</li>
+<li>
+    Эмпасофт шинэ веб сайтыг хөгжүүлсэн. / Front end, Back end/
+</li>
+<li>
+    Цаг бүртгэлийн автоматжуулалт /Power Platform/
+</li>
+<li>
+    Online сургалтын платформ /Power Platform/
+</li>
+<li>
+    Inspire 2022 арга хэмжээнд зохион байгуулагч, илтгэгч, сүлжээний
     сургалтыг удирдан хэрэгжүүлсэн.
-</p>
-<p>
-    Ø Ганада Монголиа ХХК-н бизнес процессыг Power Platform ашиглан бүрэн
+</li>
+<li>
+    Ганада Монголиа ХХК-н бизнес процессыг Power Platform ашиглан бүрэн
     автоматжуулсан.
-</p>
-<p>
-    <strong> </strong>
-</p>
-<h1>
+</li>
+</ul>
+<h2>
     Боловсрол
-</h1>
+</h2>
 <p>
-    <strong>Монгол Улсын Их Сургууль 2017 - 202</strong>
-    <strong>3</strong>
+    <strong>Монгол Улсын Их Сургууль 2017 - 2023</strong>
 </p>
 <p>
     <em>Баклавр /МУИС-ХШИУС-ЭХИТ Компьютерын сүлжээ/ </em>
 </p>
 <p>
-    <strong>Ататуркийн Нэрэмжит 5-р Сургууль</strong>
-    <strong> </strong>
-    <strong>201</strong>
-    <strong>3</strong>
-    <strong> - 201</strong>
-    <strong>7</strong>
-    <strong> </strong>
-    <strong></strong>
+    <strong>Ататуркийн Нэрэмжит 5-р Сургууль 2013 - 2017</strong>
 </p>
 <p>
     <em>Бүрэн дунд </em>
 </p>
-<p>
-    <em> </em>
-</p>
-<h1>
+<h2>
     Мэргэжлийн гэрчилгээ
-</h1>
-<p>
-    Ø
+</h2>
+<ul>
+<li>
+   
     <a
         href="https://www.credly.com/badges/41c0e9ab-428d-4e86-9fe0-67a5e9e7ba82"
     >
         Microsoft Certified: Azure Solutions Architect Expert (AZ-305)
     </a>
-</p>
-<p>
-    Ø
+</li>
+<li>
+   
     <a
         href="https://www.credly.com/badges/e16019ee-f317-48c2-95b3-b71257b6d2a3"
     >
         Microsoft Certified: Azure Administrator Associate (AZ-104)
     </a>
-</p>
-<p>
-    Ø
+</li>
+<li>
+   
     <a
         href="https://www.credly.com/badges/cafe0538-2fa7-47a0-8652-57295c2521a4/public_url"
     >
         Microsoft Power Platform Fundamentals (PL-900)
     </a>
-</p>
-<p>
-    Ø
+</li>
+<li>
+   
     <a
         href="https://www.credly.com/badges/cafe0538-2fa7-47a0-8652-57295c2521a4/public_url"
     >
         Microsoft Azure Fundamentals (AZ-900)
     </a>
-</p>
-<p>
-    Ø
+</li>
+<li>
+   
     <a
         href="https://www.credly.com/badges/145f4c1e-d391-4659-baf7-4da724d31c5e/public_url"
     >
         Microsoft Power Platform App Maker Associate (PL-100)
     </a>
-</p>
-<p>
-    Ø
+</li>
+<li>
+   
     <a
         href="https://courses.edx.org/certificates/4d44b769e7a8483aa9c080d71eb11fd5"
     >
         MITx 6.00.1x: Introduction to Computer Science and Programming Using
         Python
     </a>
-</p>
-<h1>
+</li>
+</ul>
+<h2>
     Хэл
-</h1>
-<p>
-    Ø Англи хэл (Advanced)
-</p>
-<p>
-    Ø Япон хэл (N5 түвшин)
-</p>
+</h2>
+<ul>
+<li>
+    Англи хэл (Advanced)
+</li>
+<li>
+    Япон хэл (N5 түвшин)
+</li>
+</ul>
     </div>
   )
 }
