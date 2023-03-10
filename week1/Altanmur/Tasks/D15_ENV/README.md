f08940f558a2a665fa24c812bfcbd12c62a3cb73
Altanmur Tumurbat (Алтанмөр) <altanmor@gmail.com>
2023-03-10 23:47:54 +0800

    [UPD] Final Docker-compose

--- week1/Altanmur/Tasks/D15_ENV/README.md
+++ week1/Altanmur/Tasks/D15_ENV/README.md
@@ -9,3 +9,15 @@
 ### Backend - Nest JS
 
 `http://localhost:3307`
+
+### Docker compose start
+
+`docker-compose up`
+
+### Docker remove all container
+
+`docker rm -vf $(docker ps -aq)`
+
+### Docker remove all image
+
+`docker rmi -f $(docker images -aq)`
--- week1/Altanmur/Tasks/D15_ENV/backend/src/app.controller.ts
+++ week1/Altanmur/Tasks/D15_ENV/backend/src/app.controller.ts
@@ -1,13 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('/api/nest')
+@Controller({})
 export class AppController {
   constructor(private readonly appService: AppService) {}
-  @Get()
-  getNestJS(): string {
-    return this.appService.getHello();
-  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
