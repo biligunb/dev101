9e3efc51d0a57e40378b91d8501f3f63f6c43ef0
Altanmur Tumurbat (Алтанмөр) <altanmor@gmail.com>
2023-03-10 10:54:12 +0800

    [UPD] Route /api/nest

--- week1/Altanmur/Tasks/D15_ENV/backend/src/app.controller.ts
+++ week1/Altanmur/Tasks/D15_ENV/backend/src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller({})
+@Controller('/api/nest')
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
+  @Get()
+  getNestJS(): string {
+    return this.appService.getHello();
+  }
   @Get()
   getHello(): string {
     return this.appService.getHello();
