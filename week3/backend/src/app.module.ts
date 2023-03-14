aa25c1c5c52dc147a8e0a56fde25088e1e078e24
Altanmur Tumurbat (Алтанмөр) <altanmor@gmail.com>
2023-03-14 22:35:34 +0800

    [ADD] Backend connect MySQL

--- week3/backend/src/app.module.ts
+++ week3/backend/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      // host: '172.19.0.2',
+      host: 'db',
+      port: 3306,
+      username: 'root',
+      password: 'pwd',
+      database: 'vote_db',
+      autoLoadEntities: true,
+      synchronize: true,
+      entities: [UsersModule],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
